c98705939de54f9fe99fcca6c36e14e5423c394b
Markus Dobmann <md@noqq.eu>
2015-11-17 18:50:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# react/redux login
+
+simple react login boilerplate using redux
+
+## Usage
+
+```
+$ git clone git@github.com:flootr/redux-login
+$ cd redux-login && npm install
+$ npm run dev
+```
---
+++ src/App.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import React from "react";
+
+export const App = React.createClass({
+    render () {
+        return (
+            <div>
+                <h1>Wow!</h1>
+            </div>
+        );
+    }
+});
